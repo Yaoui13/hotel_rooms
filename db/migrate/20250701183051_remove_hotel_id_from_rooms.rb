fbda522a08884601623bc6dd1da945c69b9c53cf
yacine aoui <aouiyacine1@gmail.com>
2025-07-01 21:17:56 +0200

    🔥 Suppression de la migration obsolète remove_hotel_id_from_rooms

--- db/migrate/20250701183051_remove_hotel_id_from_rooms.rb
+++
@@ -1,8 +0,0 @@
-class RemoveHotelIdFromRooms < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :rooms, :hotels
-    remove_index :rooms, :hotel_id
-    remove_column :rooms, :hotel_id
-  end
-end
-
--- db/migrate/20250701191053_remove_hotel_id_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveHotelIdFromBookings < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :bookings, :hotel_id, :bigint
-  end
-end
