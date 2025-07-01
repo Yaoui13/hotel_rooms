116d91c685dfdcbfaf15d39cecdb404c6c396a16
yacine aoui <aouiyacine1@gmail.com>
2025-07-01 19:03:28 +0200

    Fix migration add_details_to_rooms to check column existence before adding

--- db/migrate/20250619055137_add_details_to_rooms.rb
+++ db/migrate/20250619055137_add_details_to_rooms.rb
@@ -1,7 +1,23 @@
 class AddDetailsToRooms < ActiveRecord::Migration[7.0]
   def change
-    add_column :rooms, :description, :text
-    add_column :rooms, :city, :string
-    add_column :rooms, :photo_url, :string
+    unless column_exists?(:rooms, :description)
+      add_column :rooms, :description, :text
+    end
+
+    unless column_exists?(:rooms, :photo_url)
+      add_column :rooms, :photo_url, :string
+    end
+
+    unless column_exists?(:rooms, :price)
+      add_column :rooms, :price, :integer
+    end
+
+    unless column_exists?(:rooms, :capacity)
+      add_column :rooms, :capacity, :integer
+    end
+
+    unless column_exists?(:rooms, :city)
+      add_column :rooms, :city, :string
+    end
   end
 end
